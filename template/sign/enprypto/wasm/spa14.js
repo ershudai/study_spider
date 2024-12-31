2c60cbdd32d8d8adc1d58f74504ad2deedfad44c
jiangxiaolin <jiangxiaolin@ezaigc.com>
2024-12-31 17:19:14 +0800

    push

--- template/sign/enprypto/wasm/spa14.js
+++ template/sign/enprypto/wasm/spa14.js
@@ -1,7 +1,7 @@
 //https://spa14.scrape.center/page/2
 
 const fs = require('fs');
-const wasmCode=fs.readFileSync("./aa.wasm");
+const wasmCode=fs.readFileSync("./Wasm (2).wasm");
 WebAssembly.instantiate(wasmCode,{
     "env": {},
     "wasi_snapshot_preview1": {}
@@ -9,6 +9,6 @@ WebAssembly.instantiate(wasmCode,{
     const instance = result.instance;
     const exportedFunc = instance.exports
     console.log(exportedFunc); // 调用 wasm 模块中的函数
-    console.log(exportedFunc.encrypt(page, parseInt(Math.round((new Date).getTime() / 1e3).toString()))); // 调用 wasm 模块中的函数
-    console.log(exportedFunc.encrypt(page, 1727361273)); // 调用 wasm 模块中的函数
+    console.log(exportedFunc.encrypt(20, parseInt(Math.round((new Date).getTime() / 1e3).toString()))); // 调用 wasm 模块中的函数
+    console.log(exportedFunc.encrypt(20, 1727361273)); // 调用 wasm 模块中的函数
 })
